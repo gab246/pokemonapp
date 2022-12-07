bcc111600f7ec90586031ffc6378ad07aee77eef
gab246 <119339137+gab246@users.noreply.github.com>
2022-12-07 16:08:25 +0100

    added pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
 
+let pokemonList = [
+    {
+        name: 'Charmander'
+        height: 0.6
+        type: 'fire'
+    },
+    {
+        name: 'Squirtle'
+        height: 0.5
+        type: 'water'
+    },
+    {
+        name: 'Jigglypuff'
+        height: 0.5
+        type: ['fairy', 'normal']
+    }
+];
