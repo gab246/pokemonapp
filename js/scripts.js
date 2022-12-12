69280f5a3f3bd0edb8481f981678cdf2f2e1a63a
gab246 <119339137+gab246@users.noreply.github.com>
2022-12-12 11:37:33 +0100

    added loop for pokemon list and conditional for height

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,32 @@
 
 let pokemonList = [
     {
-        name: 'Charmander'
-        height: 0.6
+        name: 'Charmander',
+        height: 0.6,
         type: 'fire'
     },
     {
-        name: 'Squirtle'
-        height: 0.5
+        name: 'Squirtle',
+        height: 0.5,
         type: 'water'
     },
     {
-        name: 'Jigglypuff'
-        height: 0.5
+        name: 'Jigglypuff',
+        height: 0.5,
         type: ['fairy', 'normal']
     }
 ];
+
+
+//loop over each pokemon in the array
+for (let i = 0; i < pokemonList.length; i++){
+    console.log(pokemonList[i]);
+
+}
+
+//conditional for height of pokemons
+let height = 0.6 {
+    if (pokemon.height > 0.6)
+    console.log('Wow! That is incredibly big!');
+
+}
