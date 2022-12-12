fbe3aec022b3efed3e3bbe7e2e3e71a63914685e
gab246 <119339137+gab246@users.noreply.github.com>
2022-12-12 19:22:21 +0100

    updated for loop

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,15 @@ let pokemonList = [
     {name: 'Jigglypuff', height: 0.5,type: ['fairy', 'normal']}
 ];
 
-
-for(let i=0; i<pokemonList.length; i++){
-if (pokemonList(i).height >= 0.6){
-    console.log('Wow! That is big');
-
-}
-}
-
+  
+for (let i = 0; i < pokemonList.length; i++){
+    if (pokemonList[i].height >= 0.6){
+        document.write(
+        `${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow! That is big <br>`);
+    } else {
+        document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})<br>`);
+        
+    }
+    }
 
 
