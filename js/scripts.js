33be018357ea51d05f1b68c516fe0c73af3a60e8
gab246 <119339137+gab246@users.noreply.github.com>
2022-12-20 14:43:23 +0100

    maninpulated DOM to add function and addEventListener to each pokemon button

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,57 @@
 
-//IIFE
+// PokemonList surrounded by an IIFE
 let pokemonRespository = (function () {
+    // pokemonList defined as variable with 3 key values as objects in an array
 let pokemonList = [
     {name: 'Charmander', height: 0.6, type: 'fire'},
     {name: 'Squirtle', height: 0.5,type: 'water'},
     {name: 'Jigglypuff', height: 0.5,type: ['fairy', 'normal']}
 ];
+//returns all of the items in the pokmonList
 function getAll(){
     return pokemonList;
 }
+// function to add new pokemon to the pokemon list
 function add(pokemon){
     pokemonList.push(pokemon);
+}
+//adding pokemon to the list witht he format of a button
+function addListItem(pokemon){
+    let pokemonList= document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('button-pokemon');
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
+    //addEventListener will return pokemon info when button is clicked
+    button.addEventListener('click', function() {
+        showDetails(pokemon)
+    });
+    
+}
+function showDetails(pokemon){
+console.log(pokemon)
 }
 
 return{
 add: add,
-getAll: getAll
+getAll: getAll,
+addListItem: addListItem,
+showDetails: showDetails
+
 }
 })();
 
 
-//When adding more pokemons to the list 
-//pokemonRespository.add({name: '', height: '' ,  type: '  '})
+//forEach loop looping through the addListItem
+pokemonRespository.getAll().forEach(function (pokemon) {
+    pokemonRespository.addListItem(pokemon); 
 
-
-
-//forEach loop looping through pokemonList
-pokemonRespository.getAll().forEach(function (entry) {
-document.write(entry.name + ' ' + entry.height );
-if (entry.height >= 0.6){
-    document.write(' Wow! That´s big!');
-} 
-document.write ('<br>')
 });
   
 
-//for loop
-// for (let i = 0; i < pokemonList.length; i++){
-//     if (pokemonList[i].height >= 0.6){
-//        document.write(
-//         `${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow! That is big <br>`);
-//     } else {
-//        document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})<br>`);
+
        
         
     
