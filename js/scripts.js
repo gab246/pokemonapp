de41eb0a38668a4a9e81bce0a8d09c8753c26db1
gab246 <119339137+gab246@users.noreply.github.com>
2023-01-02 18:13:19 +0100

    added two exiting from modal options(esc key and clicking anywhere on browser)

--- js/scripts.js
+++ js/scripts.js
@@ -24,12 +24,13 @@ function addListItem(pokemon){
     let pokemonList= document.querySelector('.pokemon-list');
     let listItem = document.createElement('li');
     let button = document.createElement('button');
-    button.innerText = pokemon.name;
-    button.classList.add('button-pokemon');
+      button.innerText = pokemon.name;
+      button.classList.add('button-pokemon');
+
     listItem.appendChild(button);
     pokemonList.appendChild(listItem);
     //addEventListener will return pokemon info when button is clicked
-    button.addEventListener('click', function(event) {
+    button.addEventListener('click', function(e) {
         showDetails(pokemon)
     });
 }
@@ -56,14 +57,17 @@ function loadList() {
     return fetch(url).then(function (response) {
       return response.json();
     }).then(function (details) {
-      // Now we add the details to the item
+      // Get height, weight etc of each pokemon from api
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
-      item.types = details.types;
+      item.weight = details.weight;
+      
     }).catch(function (e) {
       console.error(e);
     });
   }
+
+  //shows modal to view pokemon details
 function showDetails (item){
     pokemonRespository.loadDetails(item).then(function(){
     let modalContainer = document.querySelector('#modal-container');
@@ -71,42 +75,67 @@ function showDetails (item){
     // Clear all existing modal content
     modalContainer.innerHTML = '';
 
+    
     //createElements to display pokemon info
     let modal = document.createElement('div');
-    modal.classList.add('modal');
+      modal.classList.add('modal');
 
     let titleElement = document.createElement('h1');
-    titleElement.innerText = item.name;
+      titleElement.innerText = item.name;
+      titleElement.classList.add('pokemonName');
 
     let detailHeight = document.createElement('p');
-    detailHeight.innerText= item.height;
+      detailHeight.innerText= item.height;
+      detailHeight.classList.add('pokemonHeight');
+      
 
-    let closeBtn = document.createElement ('button');
-    closeBtn.classList.add('modal-close')
-    closeBtn.innerText= 'close';
-    closeBtn.addEventListener('click', hideModal);
+    let detailWeight = document.createElement('p');
+      detailWeight.innerText= item.weight;
+      detailWeight.classList.add('pokemonWeight');
+
+    let closeButton = document.createElement ('button');
+      closeButton.classList.add('modal-close')
+      closeButton.innerHTML= '&times;';
+      closeButton.addEventListener('click', hideModal);
+
+  
+    let detailImg = document.createElement('img');
+      detailImg.src = item.imageUrl;
+      detailImg.classList.add('pokemonImg');
+
 
     function hideModal(){
       let modalContainer = document.querySelector('#modal-container');
       modalContainer.classList.remove('is-visible')
+
     }
-   
-
-
-
-    
-   
-
-    
+    //create all modal elements above
+    modalContainer.appendChild(modal);
+    modal.appendChild(closeButton);
     modal.appendChild(titleElement);
     modal.appendChild(detailHeight);
-    modal.appendChild(closeBtn);
-    modalContainer.appendChild(modal);
+    modal.appendChild(detailWeight);
+    modal.appendChild(detailImg);
 
     modalContainer.classList.add('is-visible');
-    
-    
 
+   
+    //esc out of modal using key
+    window.addEventListener('keydown', function(e){
+      let modalContainer= document.querySelector('#modal-container');
+      if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')){
+        hideModal();
+      }
+    });
+
+    //click out of modal
+    modalContainer.addEventListener('click', function(e){
+      let target = e.target;
+      if(target === modalContainer){
+        hideModal();
+      } 
+    })
+  
     });
 }
 
